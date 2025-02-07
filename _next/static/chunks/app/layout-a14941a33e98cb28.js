b2320b7ddc1599b0830c621fe5c5fdba9917d0c4
Angga NIX <angganix@gmail.com>
2025-02-07 11:46:51 +0700

    Deploy to GitHub Pages

---
+++ _next/static/chunks/app/layout-a14941a33e98cb28.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{8579:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,6923,23)),Promise.resolve().then(l.t.bind(l,3275,23))},3275:()=>{},6923:e=>{e.exports={style:{fontFamily:"'Poppins', 'Poppins Fallback'",fontStyle:"normal"},className:"__className_2dbf9b"}}},e=>{var s=s=>e(e.s=s);e.O(0,[672,130,215,744],()=>s(8579)),_N_E=e.O()}]);
