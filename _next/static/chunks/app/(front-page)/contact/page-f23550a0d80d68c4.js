797a2dd30b66afd143c677fe82271361ef760d6f
angganix <angganix@gmail.com>
2025-02-23 00:15:53 +0700

    Deploy to GitHub Pages

---
+++ _next/static/chunks/app/(front-page)/contact/page-f23550a0d80d68c4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[776],{7603:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,231,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[231,130,215,744],()=>s(7603)),_N_E=e.O()}]);
